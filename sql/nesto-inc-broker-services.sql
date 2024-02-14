58627e841dee87302dbdd62f6e8b93a2ae8a7ad6
AzureAD\EricChan <eric.chan@tngoc.com>
2024-02-14 11:21:27 -0500

    NE-12033

--- sql/nesto-inc-broker-services.sql
+++ sql/nesto-inc-broker-services.sql
@@ -1,36 +1,36 @@
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'APP_REQUEST_STATUS', 'Completed,Reviewed,NAS_Reviewed,Cancelled,Under_Review,NAS_Under_Review,Resubmitted,OHC,OHA,OHR', NULL);
+VALUES('Nesto Inc. Broker Services', 'APP_REQUEST_STATUS', 'Completed,Reviewed,NAS_Reviewed,Cancelled,Under_Review,NAS_Under_Review,Resubmitted,OHC,OHA,OHR', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'BILLING_DAY_OF_WEEK', 'MONTHLY', NULL);
+VALUES('Nesto Inc. Broker Services', 'BILLING_DAY_OF_WEEK', 'MONTHLY', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'BILLING_FREQUENCY', 'MONTHLY', NULL);
+VALUES('Nesto Inc. Broker Services', 'BILLING_FREQUENCY', 'MONTHLY', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'COMPANY', 'Nesto Inc. Broker Services', NULL);
+VALUES('Nesto Inc. Broker Services', 'COMPANY', 'Nesto Inc. Broker Services', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'FINANCIAL_STATUS', 'TB', NULL);
+VALUES('Nesto Inc. Broker Services', 'FILE_FORMAT', 'EXCEL', NULL);
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'INCLUDE_ZERO_VALUES', 'TRUE', NULL);
+VALUES('Nesto Inc. Broker Services', 'FINANCIAL_STATUS', 'TB', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'INVOICE_EXTENSION', 'xls', NULL);
+VALUES('Nesto Inc. Broker Services', 'INCLUDE_ZERO_VALUES', 'TRUE', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'MARK_INVOICED', 'TRUE', NULL);
+VALUES('Nesto Inc. Broker Services', 'INVOICE_EXTENSION', 'xls', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'PAYMENT_TYPES', 'N,P', NULL);
+VALUES('Nesto Inc. Broker Services', 'PAYMENT_TYPES', 'N', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'PROVINCE', 'ALL', NULL);
+VALUES('Nesto Inc. Broker Services', 'PROVINCE', 'ALL', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
 VALUES('Nesto Inc. Broker Services', 'TB_INVOICED_DATE_RANGE', '2024-02-01 00:00:00', '2024-03-01 00:00:00');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc. Broker Services', 'TEMPLATE_NAME', 'generateCustomInvoice', NULL);
+VALUES('Nesto Inc. Broker Services', 'TEMPLATE_NAME', 'distributeCustomInvoice', '');
--- sql/nesto-inc.sql
+++ sql/nesto-inc.sql
@@ -1,36 +1,39 @@
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'APP_REQUEST_STATUS', 'Completed,Reviewed,NAS_Reviewed,Cancelled,Under_Review,NAS_Under_Review,Resubmitted,OHC,OHA,OHR', NULL);
+VALUES('Nesto Inc', 'APP_REQUEST_STATUS', 'Completed,Reviewed,NAS_Reviewed,Cancelled,Under_Review,NAS_Under_Review,Resubmitted,OHC,OHA,OHR', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'BILLING_DAY_OF_WEEK', 'MONTHLY', NULL);
+VALUES('Nesto Inc', 'BILLING_DAY_OF_WEEK', 'MONTHLY', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'BILLING_FREQUENCY', 'MONTHLY', NULL);
+VALUES('Nesto Inc', 'BILLING_FREQUENCY', 'MONTHLY', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'COMPANY', 'Nesto Inc.', NULL);
+VALUES('Nesto Inc', 'COMPANY', 'Nesto Inc.', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'FINANCIAL_STATUS', 'TB', NULL);
+VALUES('Nesto Inc', 'FILE_FORMAT', 'EXCEL_PDF', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'INCLUDE_ZERO_VALUES', 'TRUE', NULL);
+VALUES('Nesto Inc', 'FINANCIAL_STATUS', 'TB', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'INVOICE_EXTENSION', 'xls', NULL);
+VALUES('Nesto Inc', 'INCLUDE_ZERO_VALUES', 'TRUE', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'MARK_INVOICED', 'TRUE', NULL);
+VALUES('Nesto Inc', 'INVOICE_EXTENSION', 'xls', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'PAYMENT_TYPES', 'N,P', NULL);
+VALUES('Nesto Inc', 'OUTPUT_FILE_FORMAT', 'EXCEL', NULL);
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'PROVINCE', 'ALL', NULL);
+VALUES('Nesto Inc', 'PAYMENT_TYPE', 'N', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'TB_INVOICED_DATE_RANGE', '2024-02-01 00:00:00', '2024-03-01 00:00:00');
+VALUES('Nesto Inc', 'PROVINCES', 'ALL', '');
 INSERT INTO appraisal.auto_invoice_criteria
 (client_company, criteria, criteria_value1, criteria_value2)
-VALUES('Nesto Inc.', 'TEMPLATE_NAME', 'generateCustomInvoice', NULL);
+VALUES('Nesto Inc', 'TB_INVOICED_DATE_RANGE', '2023-03-01 00:00:00', '2023-09-01 00:00:00');
+INSERT INTO appraisal.auto_invoice_criteria
+(client_company, criteria, criteria_value1, criteria_value2)
+VALUES('Nesto Inc', 'TEMPLATE_NAME', 'distributeCustomInvoice', '');
